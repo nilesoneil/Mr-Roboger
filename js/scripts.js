89d26fc67ecab6ddeb13a514182eebf913ca64d7
Niles O'Neil <nilesoneil@gmail.com>
2022-02-18 11:25:09 -0800

    created first array for beep

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,10 @@
-$(document).ready(function() {
-  $("form#pastry").submit(function(event) {
-    const age = $("input:radio[name=millenial/boomer]:checked").val();
-    const gender = $("select#gender").val();
+function mrRobo() {
+  inputNumber = []
+  for (i=0; i< input +1; i++) {
+    let userInput = i
+    if i.contains("1") {
+      userInput = "beep"
 
-    if (gender === 'male' && age === 'younger') {
-    $("#result1").show();
-    $("#result2").hide();
-    $("#result3").hide();
-    $("#result4").hide();
-    } else if (gender === "female") {
-    $("#result1").hide();
-    $("#result2").show();
-    $("#result3").hide();
-    $("#result4").hide();
-    } else if (age === "older" && gender === "male") {
-    $("#result1").hide();
-    $("#result2").hide();
-    $("#result3").show();
-    $("#result4").hide();
-    } else if (gender === "female") {
-    $("#result1").hide();
-    $("#result2").hide();
-    $("#result3").hide();
-    $("#result4").show();
     }
-
-    event.preventDefault();
-  });
-});
+  }
+}
